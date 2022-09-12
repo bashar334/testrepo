2c96fcde4756d2538e390060f25c730d194e0f89
bashar334 <ba7992@rit.edu>
2022-09-12 18:02:58 +0400

    test test

---
+++ OneDrive/Desktop/New folder/lol.py
@@ -0,0 +1,69 @@
+import turtle
+
+
+def rectangle():
+    turtle.down()
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.done
+ 
+    
+    
+
+
+
+
+def triangle():
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(50)
+    turtle.forward(65)
+    turtle.left(80)
+    turtle.forward(70)
+   
+
+
+
+
+def house():
+    rectangle()
+    triangle()
+    
+
+house()
+
+turtle.up()
+turtle.home()
+turtle.backward(200)
+house()
+
+
+def lollipop():
+    turtle.up()
+    turtle.home()
+    turtle.forward(100)
+    turtle.down()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.right(180)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.circle(radius=60)
+
+
+
+lollipop()
+
+
